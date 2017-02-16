59e43128d618168302bfab04f60c6490f0038c11
Catherin Jean <catherinejeanbean@gmail.com>
2017-02-15 18:42:18 -0600

    finish gps files with guide in CSS and add ruby pairing 1 file

---
+++ ruby/hamsters.rb
@@ -0,0 +1,66 @@
+puts "Name of hampster?"
+
+hamster_name = gets.chomp
+
+puts "Name of hampster is #{hamster_name}"
+
+puts "volume level from 1 to 10 of #{hamster_name}"
+volume_level = gets.chomp
+puts "volume level of hampster #{hamster_name} is #{volume_level}"
+
+
+
+
+valid_input = false
+until valid_input
+
+	puts "Fur color of hamster is?"
+fur_color = gets.chomp
+	
+
+if fur_color =="brown"
+puts "fur color of #{hamster_name} is brown"
+valid_input = true
+elsif fur_color =="white"
+	puts "fur color of #{hamster_name} is white"
+valid_input = true
+elsif fur_color == "tan"
+	puts "fur color of #{hamster_name} is tan"
+	valid_input = true
+else 
+	puts "did not understand try: brown, white or tan"
+
+end
+end
+
+
+good_candidate = false
+until good_candidate
+
+	puts "Is  #{hamster_name} a good canadate for addoption (y,n,maybe)?"
+addoption = gets.chomp
+	
+
+if addoption =="y"
+puts "Great! Lets find #{hamster_name} a home!"
+good_candidate = true
+elsif addoption =="n"
+	puts "Oh no!"
+good_candidate = true
+elsif addoption == "maybe"
+	puts "Yay there is a chance for #{hamster_name}"
+	good_candidate = true
+else 
+	puts "Learn how to read!!"
+
+end
+end
+
+puts "if age -""
+
+
+
+
+
+
+
