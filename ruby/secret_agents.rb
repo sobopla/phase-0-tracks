2c2166a3b267cecfa0f0359c77d2d68dc206e73e
Catherin Jean <catherinejeanbean@gmail.com>
2017-02-19 15:29:40 -0600

    fix indentation

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,19 +1,19 @@
 
 # W E I T Z E N K O R N 
 # 0 1 2 3 4 5 6 7 8 9 10
-#psuedocode
-#need a encrypt method
-#that advances every letter of a string one letter forward
+# psuedocode
+# need a encrypt method
+# that advances every letter of a string one letter forward
 # index = 0
 # (advance string letter forward)
 # define index in password
 # advance password.index +1 (forward) 
 # if space is entered then space is equal to an index and not a !
 # space will equal the password index (be a part of it's index)
-#decrypt
-#define the index of the alphabet using a string "abcdefghijklmnopqrstuvwxyz"
-#equate the alphabet index to the password index number
-#move the password index to the alphabet index before the password index in the alphabet
+# decrypt
+# define the index of the alphabet using a string "abcdefghijklmnopqrstuvwxyz"
+# equate the alphabet index to the password index number
+# move the password index to the alphabet index before the password index in the alphabet
 
 # index += 1
 
@@ -23,9 +23,9 @@ def encrypt(password)
 	
 		if password[index] == " "
 			password[index] = password[index] 
-		  elsif password[index] == "z" 
-      password[i] = "a" 
-		  else
+		elsif password[index] == "z" 
+      	password[i] = "a" 
+		else
 			password[index] = password[index].next 
 		end
 	  index += 1
@@ -63,11 +63,11 @@ end
 
 	 	# another way
 	 	# else 
-	 	# 	letter = password[index] 
-	 	#  	alphabet = "abcdefghijklmnopqrstuvwxyz"
-	 	#  	letter_spot = alphabet.index(letter) 
-	 	#    	previous_letter = alphabet[letter_spot-1] 
-	 	#    	password[index] = previous_letter
+	 	# letter = password[index] 
+	 	# alphabet = "abcdefghijklmnopqrstuvwxyz"
+	 	# letter_spot = alphabet.index(letter) 
+	 	# previous_letter = alphabet[letter_spot-1] 
+	 	# assword[index] = previous_letter
 
  	#put in alphabet, call the indexes, index = the one before it
 
