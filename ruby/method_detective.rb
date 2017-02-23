e6dd07a687dee4da20487f0ee20fd918e371055d
Catherin Jean <catherinejeanbean@gmail.com>
2017-02-22 20:18:21 -0600

    save 5.1 with data in it

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,55 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".insert(1, 'o')
 # => “zoom”
+#"zoom".squeeze####not relevant"
 
-# "enhance".<???>
+	# "enhance".center(4)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+	# "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
+#
 
-# "the usual".<???>
+#!!!!####BEST#### 
+#"the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+#!!!!####BEST#### 
+#" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+#"The case of the disappearing last letter".chomp('r')
 # => "The case of the disappearing last lette"
+#"The case of the disappearing last letter".chop
+#!!!!####BEST#### 
+#"The case of the disappearing last letter".slice(0..-2)
 
-# "The mystery of the missing first letter".<???>
+# "The mystery of the missing first letter".delete('T')
 # => "he mystery of the missing first letter"
+#!!!!####BEST####
+#"Try the mystery of the first letter".slice(1..-1)
 
-# "Elementary,    my   dear        Watson!".<???>
+#####NOTES#####
+#.slice(-1) gets the last letter
+#.slice(0) gets the first letter 
+
+
+# "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+#!!!!####BEST####
+#"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+#!!!!####BEST####
+#122 is a decimal representation of the character "z"
 
-# "How many times does the letter 'a' appear in this string?".<???>
+#!!!!####BEST####
+#"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
+
