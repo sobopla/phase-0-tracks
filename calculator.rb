2b49ecbf040486efa0d8c85196a9b15542716a61
Catherin Jean <catherinejeanbean@gmail.com>
2017-02-27 12:41:33 -0600

    work out 5, 6

--- calculator.rb
+++ calculator.rb
@@ -8,15 +8,29 @@
 #question 3
 # prompt user for calculation
 loop do
-  
+  holder = []
   puts "enter no 1 or done"
 a = gets.chomp
-if a == "done"
-  break
-else
-puts "enter no 2"
-b = gets.chomp.to_i
-puts a.to_i + b
-
-   
+	if a == "done"
+  		break
+	else
+		puts "enter no 2"
+		b = gets.chomp.to_i
+		ans = a.to_i + b
+	puts ans
+holder << a
+holder << b
+p holder
+puts "#{holder[0]} + #{holder[1]} = #{ans}"
+#somehow store next answers in a new array and print history...?
 end
+end
+#question 5 psuedocode
+#shovel a into a new array each time
+#shovel b into the same new array
+# to print out the array with it's integer for the calculation performed
+#the index of a, and the index of b added equals
+
+#question 6 psuedocode
+#if an integer is not entered , put wrong input , enter a number
+
