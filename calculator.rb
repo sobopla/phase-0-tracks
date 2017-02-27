682adec43db04326c6a1ace7ee4a91475ee8f132
Catherin Jean <catherinejeanbean@gmail.com>
2017-02-27 12:04:50 -0600

    start assesment

---
+++ calculator.rb
@@ -0,0 +1,22 @@
+# a = 5
+# b ="word"
+# c = 2 
+# def add(a,b,c)
+#   puts a + b.to_i + c
+# end
+
+#question 3
+# prompt user for calculation
+loop do
+  
+  puts "enter no 1 or done"
+a = gets.chomp
+if a == "done"
+  break
+else
+puts "enter no 2"
+b = gets.chomp.to_i
+puts a.to_i + b
+
+   
+end
