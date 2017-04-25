a5a5ea03b688d1ad426076bab24806b12d0a6428
Catherin Jean <catherinejeanbean@gmail.com>
2017-04-25 16:57:47 -0500

    create code for 5.6

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,39 +1,30 @@
-#psuedocode
-
-#Release 0
-array = [11,22,33,44,55,]
-def search_array (array, integer)
+arr =[11, 22, 33, 44, 55]
+def search_array(arr, num)
 	return_index = nil
-	index_place = 0
-		array.each do |num|
-			if array[index_place] == integer
-				return_index = index_place
-			else
-				index_place += 1
+	index = 0
+		arr.each do |x|
+			if arr[index] == num
+				return_index == index
 			end
-		end
+			index += 1
 	return_index
-	end
-p search_array(array,30)
+end
 
-
-
-fib_array = [0,1]
-
-
-#puts "what's your fib number"
-#num = gets.chomp
 def fib(num)
   fib_array = [0,1]
   until fib_array.length == num
-  number = (fib_array[-2] + fib_array[-1])
-  fib_array.push(number)
+  x = (fib_array[-2] + fib_array[-1])
+  fib_array.push(x)
   end
-  p fib_array
+  fib_array
 end
 
-fib(100)
+# take an array of numbers
+# compare the first number (or index 0 ) with the next number (the next index)
+# and put the lesser number first 
+# 	in other words swap the numbers if the first number is higher
+# 	then make a new array with the compared numbers 
+# 	until no numbers in the array are swapped continue
 
-#didn't get to the last part with my pair. 
-
-
+# while the numbers compared need to be swapped, run the command
+# 	break if the numbers don't need to be swapped , then the sort is done
