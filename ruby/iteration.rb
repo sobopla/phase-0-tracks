1ae809fa83cac24d09f44b6a1aa881236152f704
Catherin Jean <catherinejeanbean@gmail.com>
2017-04-26 14:51:13 -0500

    add print statements to test and organize

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -9,65 +9,83 @@ music_list { |genre1, genre2| puts "You can listen to #{genre1} or #{genre2}"}
 
 
 # declare an array and a hash, with some data. 
-# .each on an array
-# .each on a hash
-# .map! on array
-# no map! on hash
-# Demonstrate that you can iterate through each one using .each, and then using .map!
-#  (modifying the data in some way). 
-#  Note that you can't call .map! on a hash, so you can skip that  
-# Print the data structures before and after each
-#  call to demonstrate whether they have been modified or not.
 
+#-----------------ARRAY------------------:
 array = [1, 2, 3, 4, 5]
 new_array = []
 
-#array with  .each
-array.each do  |num| num.next
+#array with .each
+array.each do  |num| num.next #.next does not change original array
 	new_array << num.next
 end
+p "---original array---"
 p array
-new_array
+p "new_array of next number:"
+p new_array
+
+#array with map and no !
+#this won't work (I wanted to see what it did):
+# array.map do |num| num += 2
+# 	new_array << num.map 
+# end
+ 
 
 #array with .map
-p array #to show original array before map changes it
+p "---original array---" #.map! permanently changes original
+p array 
 array.map! do |num|
 	num += 3
 end
+p "array + 3:"
 p array #altered array
 
 #array with a delete 
 #?? is this the same as a filter?
-array.delete_if {|i| i < 8}
-puts "----"
+p "array is now:"
+p array
+array.delete_if {|i| i < 2}
+puts "array deleting num less than 5:"
 p array
 
+#filter method for array
+p "array is now:"
+p array
+p "filter array with select greater than 3:"
+array.select{|num| num >= 3}
+p array
 
-#HASH
+#---------------HASH-----------------------
 
 cities = {texas: "austin", illinois: "chicago", california: "sacramento"}
-#hash with each
-#update city
+
+#hash with each to update city
 
 cities.each do |state, city|
 	puts "#{city.capitalize} is in #{state}"
 end
+p "cities each capitalized:"
 p cities
 
 #method that deletes 
 def delete_texas(places)
 	places.delete_if {|state, city| state == :texas}
 end
-delete_texas(cities)
-p "63"
-p cities
 
+delete_texas(cities)
+p "cities without texas"
+p cities
+#or to not change the original:
+non_texas = cities.delete_if {|state, city| state == :texas}
+p "non_texas one line for variable code test:"
+p non_texas
 #filter for a hash
 #select
 non_illinois = cities.select do |state, city|
 	state != :illinois
 end
+p "cities not in Illinois:"
 p non_illinois
+p "original cities"
 
 #4. method that will iterate over numbers in an array
 # if the numbers are even , then its true
