15dd8b5b673f1e40c8675d17a195060261b900aa
Catherin Jean <catherinejeanbean@gmail.com>
2017-04-27 14:45:01 -0500

    write .each method for arr and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -2,3 +2,13 @@
 
 bands = ["the stooges","love", "the flying burrito brothers", "jefferson airplane", "fleetwood mac"]
 groups = {fleetwood_mac: "stevie nicks", rolling_stones: ["kieth richards","mick jagger"], flying_burrito_brothers: "gram parsons"}
+
+#.each
+#a
+bands.each do |band|
+	p band
+end
+#h
+groups.each do |band, singer|
+	puts "A singer in #{band.to_s.sub(/[_]/, " ")} is #{singer}"
+end #don't know how to get rid of a second '_' (until .include? "_" = false, puts string?)
