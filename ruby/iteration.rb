e08e9c311e184111dba4edd503d214866f08c1c9
Catherin Jean <catherinejeanbean@gmail.com>
2017-04-27 14:44:09 -0500

    create array and hash with meaningful data

---
+++ ruby/iteration.rb
@@ -0,0 +1,4 @@
+#RELEASE 1
+
+bands = ["the stooges","love", "the flying burrito brothers", "jefferson airplane", "fleetwood mac"]
+groups = {fleetwood_mac: "stevie nicks", rolling_stones: ["kieth richards","mick jagger"], flying_burrito_brothers: "gram parsons"}
