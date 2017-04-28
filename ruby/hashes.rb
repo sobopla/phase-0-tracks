09ea971b0cea7c03691272f74a243cd58ac9f2c1
Catherin Jean <catherinejeanbean@gmail.com>
2017-04-27 19:20:42 -0500

    psuedocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+#------------- CLIENT DETAILS --------------
+#-----psuedocode
+#enter client info: 
+#name gets chomp name into string
+#age gets age to i
+#number of rooms to i
+#color theme to string
+#color bright to true/false
+#color muted to true/false
+
+#ask user to update a color theme
+#turn color theme into a symbol
+#print the input as a hash
+
+#ask user if they want to update other fields
+#turn answer into a symbol
+#get update data from user
+#make the symbol update the key in the hash with the data as the value
+
