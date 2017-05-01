cff746cbb8e1b35fcbce237ff23717d5f57cc6df
Catherin Jean <catherinejeanbean@gmail.com>
2017-04-30 22:34:40 -0500

    add make alias meth

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -17,3 +17,33 @@ def cap(name)
   name.split(' ').each {|name| name.capitalize!}.join(' ') 
 end
 
+#reverse name
+def reverse(name)
+  name.split(' ').reverse.join(' ').chars
+end
+
+#make alias method  
+def convert(name)
+  vowels = ["a", "e", "i", "o", "u"]
+  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+  n = name.length 
+ 
+  reversed = reverse(name)
+  
+  
+  reversed.map! do |char| 
+    if char == "u" 
+      char = "a"
+    elsif  char == "z"
+      char = "b"
+    elsif vowels.include?(char) 
+      char = vowels[vowels.index(char) + 1]
+    elsif consonants.include?(char)
+      char = consonants[consonants.index(char) + 1]
+    else char = char
+    end
+  end
+           
+  reversed.join('')
+  
+end 
