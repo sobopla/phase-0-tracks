7cc4bb58de5ae7bfb4c1e308becd243404227530
Catherin Jean <catherinejeanbean@gmail.com>
2017-05-02 19:44:44 -0500

    write puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,36 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def initialize
+		puts "Initializing new puppy instance..."
+	end
+
+  	def fetch(toy)
+	    puts "I brought back the #{toy}!"
+	    toy
+  	end
+
+  	def speak(i)
+  		puts "Woof!" * i
+	end
+
+	def rollover
+		puts "rolls over"
+	end
+
+	def dogyears(human_years)
+		human_years * 7
+	end
+
+	def run(i)
+		puts "running round in a circle " * i
+	end
 
 end
 
+
+#DRIVER CODE
+ralph = Puppy.new
+ralph.run(7)
+ralph.rollover
+ralph.speak(4)
+ralph.fetch("Giant teddy bear")
