64dd1c3bd8c39784fbabefbd0896940fea0554e8
Catherin Jean <catherinejeanbean@gmail.com>
2017-05-05 18:12:07 -0500

    test make_display_board

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -3,12 +3,15 @@ require_relative 'game'
 describe Game do
   let(:game) {Game.new("bitty kitty")}
 
-  it "makes a board hiding letters with _ " do
+  it "makes a board array replacing letters with _ " do
   expect(game.make_board).to eq ["_", "_", "_", "_", "_", " ", "_", "_", "_", "_", "_"]
   end
 
 
-
+  it "joins board array with spaces to show user the board in a string" do
+  game.make_board
+  expect(game.make_display_board).to eq "_ _ _ _ _   _ _ _ _ _"
+  end
 
 
 
