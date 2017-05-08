6560b9528f5b638f37979f6968d5e0644b315187
Catherin Jean <catherinejeanbean@gmail.com>
2017-05-07 20:58:48 -0500

    test new classes with Module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -10,6 +10,27 @@ module Shout # add method for yelling angrily and yelling_happily
   end
 
 end
+# # Add driver code underneath your module declaration that calls both of your module methods.
+# p Shout.yell_angrily('words')
+# p Shout.yelling_happily('happy happy')
 
-p Shout.yell_angrily('words')
-p Shout.yelling_happily('happy happy')
+# 1. Comment out the old code in shout.rb, and make a mixin version of the Shout module instead.
+# 
+module Shout
+  def shout(song)
+    puts '```' + song.upcase + '```'
+  end
+
+class Sixties_Songs # 2. Write two classes representing anything that might shout, and include the Shout module in those classes.
+  include Shout
+end
+
+class Eighties_Songs
+  include Shout
+end
+
+isley_brothers = Sixties_Songs.new
+isley_brothers.shout("you know you make me wanna shout")
+
+tears_for_fears = Eighties_Songs.new
+tears_for_fears.shout("Shout shout let it all out..")
