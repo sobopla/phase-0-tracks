b52b285900658949ba0a7d702a853b7f0418ef89
Catherin Jean <catherinejeanbean@gmail.com>
2017-05-21 23:57:35 -0500

    Get faker date to work

--- databases/githappy/mood_tracker.rb
+++ databases/githappy/mood_tracker.rb
@@ -12,6 +12,8 @@
 # require gems
 require 'sqlite3'
 require 'date'
+require 'faker'
+
 
 # create SQLite3 database
 mood_database = SQLite3::Database.new( "moods.db" )
@@ -42,17 +44,20 @@ p moods
 
 
 # create method for mood entry
-def create_mood_entry(db, date, mood, activity, food) #think about adding location for a 'you should move to _____ method'
-  date = Time.now
-  month = date.month
-  day = date.day
-  year = date.year
+def create_mood_entry( mood_database, month, day, year, mood, activity, food) #think about adding location for a 'you should move to _____ method'
+  # WILL NEED TO BE UNCOMMENTED FOR THE REAL PROGRAM
+  # date = Time.now
+  # month = date.month
+  # day = date.day
+  # year = date.year
   mood_database.execute("INSERT INTO moods (month, day, year, mood, activity, food) VALUES (?, ?, ?, ?, ?, ?)", [month, day, year, mood, activity, food])
 end 
 
-10.times do #create a lot of mood entries using Faker::date / figure out how to set variable to faker date and get month/day/yr
-  create_mood_entry()
+#past = Faker::Date.backward(14)
 
+10.times do #create a lot of mood entries using Faker::date / figure out how to set variable to faker date and get month/day/yr
+  create_mood_entry(mood_database, Faker::Date.backward(90).month, Faker::Date.backward(90).day, Faker::Date.backward(90).year, "happy", "dancing", "wine")
+end
 
 =begin # not sure if this will work
 
