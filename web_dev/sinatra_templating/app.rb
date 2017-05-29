b658ebbab1abf12662549642dfb648375d866551
Catherin Jean <catherinejeanbean@gmail.com>
2017-05-29 17:04:59 -0500

    Rename confusing students to class

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -8,31 +8,31 @@ db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
 # show students on the home page
-# get '/' do
-# #? WHY DOESN'T IT WORK WHEN THIS IS COMMENTED OUT
+ get '/' do
+#? WHY DOESN'T IT WORK WHEN THIS IS COMMENTED OUT
 
-#   @students = db.execute("SELECT * FROM students")
-#   p @students
-#   p params
-#   erb :home
-# end
+  @students = db.execute("SELECT * FROM students")
+  p @students
+  p params
+  erb :home
+end
 
-get '/students/new' do
+get '/class/new' do
   erb :new_student
 end
 
-get '/' do 
+get '/class/diets' do 
 	@students = db.execute("SELECT * FROM students")
-	p @students
+	#p @students
 	erb :index
 end
 
 # create new students via
 # a form
-post '/students' do
+post '/class' do
   db.execute("UPDATE students SET diet = ? WHERE name = ?", [params['diet'], params['name']])
-  @students = db.execute("SELECT name FROM students WHERE diet = 'vegan'")
-  redirect '/'
+  #@students = db.execute("SELECT name FROM students WHERE diet = 'vegan'")
+  redirect '/class/diets'
 end
 
 # add static resources
